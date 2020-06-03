8d6950d598a00bce89c8e934b81ff2a76dda3cdf
João Vitor Dutra <jo.vitor.d@gmail.com>
2020-06-03 17:20:10 -0300

    Adjust responsive modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const searchButton = document.querySelector("#page-home main a");
 const modal = document.querySelector("#modal");
+const closeButton = document.querySelector("#modal .header a");
 
-searchButton.addEventListener("click", () => {
-  modal.classList.toggle("hide");
-});
+searchButton.addEventListener("click", () => modal.classList.remove("hide"));
+
+closeButton.addEventListener("click", () => modal.classList.add("hide"));
