86ac32ab25f145abba268640a245aaf8f43bc3cd
João Vitor Dutra <jo.vitor.d@gmail.com>
2020-06-02 22:43:33 -0300

    Finish populateUFs on Form
    This JavaScript function fetches from the IBGE REST API to populate the form with all 27 Brazilian States.

---
+++ scripts/create-point.js
@@ -0,0 +1,16 @@
+function populateUFs() {
+  const ufSelect = document.querySelector("select[name=uf]");
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then((response) => response.json())
+    .then((states) => {
+      for (const state of states) {
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+      }
+    });
+}
+
+populateUFs();
+
+document.querySelector("select[name=uf]").addEventListener("change", () => {
+  console.log("mudei");
+});
