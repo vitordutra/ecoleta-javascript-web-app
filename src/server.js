cc0e1da11d7cda0869d4a59180c2771c3eaf540c
João Vitor Dutra <jo.vitor.d@gmail.com>
2020-06-04 19:20:08 -0300

    Make express Hello World

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const server = express();
 
+// Configurar os caminhos da minha aplicação
+// Página Inicial
+server.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 // Ligar o servidor
 server.listen(3000);
