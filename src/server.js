ac957a5c740d1a0ccaeeb4fecfdfab86572c73b1
João Vitor Dutra <jo.vitor.d@gmail.com>
2020-06-04 19:30:58 -0300

    Want to Reset HTML

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const server = express();
 
+// Configurar pasta pública
+server.use(express.static("public"));
+
 // Configurar os caminhos da minha aplicação
 // Página Inicial
 server.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // Ligar o servidor
