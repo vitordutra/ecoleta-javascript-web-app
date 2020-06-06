680ac8a90529176009f8eb7f5ff7ca6114b02fa4
João Vitor Dutra <jo.vitor.d@gmail.com>
2020-06-06 15:33:53 -0300

    Pomodoro #1

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,39 @@
 // Importar a dependência do sqlite3
 
-const sqlite3 = require("sqlite3").verbose;
+const sqlite3 = require("sqlite3").verbose();
 
-// Croar p pnketp qie irá fazer operações no banco de Dados
+// Criar p pnketp qie irá fazer operações no banco de Dados
 const db = new sqlite3.Database("./src/database/database.db");
+
+//Ultilizar o objeto de banco de dados para as nossas operações
+db.serialize(() => {
+  // Criar uma tabela com comandos SQL
+  db.run(`
+        CREATE TABLE IF NOT EXISTS places(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+        );
+
+    `);
+  // Inserir dados na tabela
+  db.run(`
+        INSERT INTO places (image,
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+            ) VALUES (?,?,?,?,?,?,?);
+    `);
+  // Consultar os dados na tabela
+
+  // Deletar um dado na tabela
+});
