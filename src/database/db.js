6263a478ce04e655f831ce4d87b04be8ae9e8d39
João Vitor Dutra <jo.vitor.d@gmail.com>
2020-06-06 17:11:20 -0300

    Pomodoro #3

--- src/database/db.js
+++ src/database/db.js
@@ -8,69 +8,69 @@ const db = new sqlite3.Database("./src/database/database.db");
 module.exports = db;
 
 //Utilizar o objeto de banco de dados para as nossas operações
-//  db.serialize(() => {
-// //   // Criar uma tabela com comandos SQL
-// // //   db.run(`
-// // //         CREATE TABLE IF NOT EXISTS places(
-// // //             id INTEGER PRIMARY KEY AUTOINCREMENT,
-// // //             image TEXT,
-// // //             name TEXT,
-// // //             address TEXT,
-// // //             address2 TEXT,
-// // //             state TEXT,
-// // //             city TEXT,
-// // //             items TEXT
-// // //         );
+db.serialize(() => {
+  //   // Criar uma tabela com comandos SQL
+  // //   db.run(`
+  // //         CREATE TABLE IF NOT EXISTS places(
+  // //             id INTEGER PRIMARY KEY AUTOINCREMENT,
+  // //             image TEXT,
+  // //             name TEXT,
+  // //             address TEXT,
+  // //             address2 TEXT,
+  // //             state TEXT,
+  // //             city TEXT,
+  // //             items TEXT
+  // //         );
 
-// // //     `);
-// //   // Inserir dados na tabela
-// //   const query = `
-// //         INSERT INTO places (
-// //             image,
-// //             name,
-// //             address,
-// //             address2,
-// //             state,
-// //             city,
-// //             items
-// //             ) VALUES (?,?,?,?,?,?,?);
-// //     `;
+  // //     `);
+  //   // Inserir dados na tabela
+  //   const query = `
+  //         INSERT INTO places (
+  //             image,
+  //             name,
+  //             address,
+  //             address2,
+  //             state,
+  //             city,
+  //             items
+  //             ) VALUES (?,?,?,?,?,?,?);
+  //     `;
 
-// //   const values = [
-// //     "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1401&q=80",
-// //     "Papersider",
-// //     "Guilherme Gemballa, Jardim América",
-// //     "Número 260",
-// //     "Santa Catarina",
-// //     "Rio do Sul",
-// //     "Papeis e Papelão",
-// //   ];
+  //   const values = [
+  //     "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1401&q=80",
+  //     "Papersider",
+  //     "Guilherme Gemballa, Jardim América",
+  //     "Número 260",
+  //     "Santa Catarina",
+  //     "Rio do Sul",
+  //     "Papeis e Papelão",
+  //   ];
 
-// //   function afterInsertData(err) {
-// //     if (err) {
-// //       return console.log(err);
-// //     }
-// //     console.log("Cadastrado com Sucesso");
-// //     console.log(this);
-// //   }
+  //   function afterInsertData(err) {
+  //     if (err) {
+  //       return console.log(err);
+  //     }
+  //     console.log("Cadastrado com Sucesso");
+  //     console.log(this);
+  //   }
 
-// //   db.run(query, values, afterInsertData);
+  //   db.run(query, values, afterInsertData);
 
-// //   // Consultar os dados na tabela
+  //   // Consultar os dados na tabela
 
-// //   db.all(`SELECT * FROM places;`, function (err, rows) {
-// //     if (err) {
-// //       console.log(err);
-// //     }
-// //     console.log("Aqui estão seus registros: ");
-// //     console.log(rows);
-// //   });
+    // db.all(`SELECT * FROM places;`, function (err, rows) {
+    //   if (err) {
+    //     console.log(err);
+    //   }
+    //   console.log("Aqui estão seus registros: ");
+    //   console.log(rows);
+    // });
 
-// //   //   Deletar um dado na tabela
-// //   //   db.run(`DELETE FROM places WHERE id = ?`, [1], function (err) {
-// //   //     if (err) {
-// //   //       console.log(err);
-// //   //     }
-// //   //     console.log("Registro deletado com sucesso");
-// //   //   });
-// });
+  // Deletar um dado na tabela
+//   db.run(`DELETE FROM places WHERE id = ?`, [3], function (err) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("Registro deletado com sucesso");
+//   });
+});
